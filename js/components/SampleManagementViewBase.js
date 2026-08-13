3b920d50d45c8279568322cf315c48bcf274c4c8
Ilias Bennani <ilias.bennani@gmail.com>
2026-08-13 07:41:39 +0200

    Förbered kompatibel brygga för utökad samplevy

---
+++ js/components/SampleManagementViewBase.js
@@ -0,0 +1,5 @@
+/**
+ * Tunn JS-brygga till den befintliga manuella samplevyn.
+ * Hålls separat så att utökningar kan skrivas i TypeScript utan JS -> TS-importer.
+ */
+export * from './SampleManagementViewComponent.ts';
